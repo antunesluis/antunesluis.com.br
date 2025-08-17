370b2ebad0b7e5d1836d7fefc9ebeb132a8fe01f
Luis Antunes <luisantunes0914@gmail.com>
2025-08-17 01:56:18 -0300

    feat(components): adding ImageUploader component

--- src/components/admin/ImageUploader/index.tsx
+++ src/components/admin/ImageUploader/index.tsx
@@ -1,10 +1,10 @@
 'use client';
 
 import { Button } from '@/components/Button';
+import { IMAGE_UPLOAD_MAX_SIZE } from '@/lib/constants';
 import { ImageUpIcon } from 'lucide-react';
 import { useRef } from 'react';
-
-// type ImageUploaderProps = {};
+import { toast } from 'react-toastify';
 
 export function ImageUploader() {
   const fileInputRef = useRef<HTMLInputElement>(null);
@@ -12,6 +12,30 @@ export function ImageUploader() {
   function handleChoseFile() {
     if (!fileInputRef.current) return;
     fileInputRef.current.click();
+  }
+
+  function handleChange() {
+    if (!fileInputRef.current) return;
+
+    const fileInput = fileInputRef.current;
+    const file = fileInput?.files?.[0];
+
+    if (!file) return;
+
+    if (file.size > IMAGE_UPLOAD_MAX_SIZE) {
+      const readableMaxSize = IMAGE_UPLOAD_MAX_SIZE / 1024;
+      toast.error(`Image too large. Maximum size is ${readableMaxSize}KB.`);
+
+      fileInput.value = '';
+      return;
+    }
+
+    const formData = new FormData();
+    formData.append('file', file);
+
+    // TODO: Implement the upload logic here
+
+    fileInput.value = '';
   }
 
   return (
@@ -28,6 +52,7 @@ export function ImageUploader() {
       </Button>
 
       <input
+        onChange={handleChange}
         ref={fileInputRef}
         className='hidden'
         name='file'
--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,1 +1,3 @@
 export const SIMULATE_WAIT_TIME = 100;
+
+export const IMAGE_UPLOAD_MAX_SIZE = 1024 * 900;
