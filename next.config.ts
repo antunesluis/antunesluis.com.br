451b0b1d3688746126ee2e260705d536c0a627bc
Luis Antunes <luisantunes0914@gmail.com>
2025-09-09 18:10:00 -0300

    add antunesluis.com.br in next.config.js

--- next.config.ts
+++ next.config.ts
@@ -15,6 +15,12 @@ const nextConfig: NextConfig = {
         port: '3000',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'antunesluis.com.br',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
